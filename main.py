65d48b387fa0d8ea7f6e170223189027b8331c3b
murali2p <mohanmurali.behera@gmail.com>
2024-06-04 22:07:39 +0530

    first docker attempt

---
+++ main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def testing():
+    return "Welcome to docker"
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", host="0.0.0.0", port=80, reload= True)
